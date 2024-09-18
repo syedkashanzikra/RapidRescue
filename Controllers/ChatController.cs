ae8fd2ad0458d11f24373a5c5869b7641e324202
Headbreakerr <133881860+Headbreakerr@users.noreply.github.com>
2024-09-18 15:26:43 +0500

    Chat Template Integrate and cleaning

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidRescue.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            var breadcrumbs = new List<Tuple<string, string>>()
+    {
+        new Tuple<string, string>("Home", Url.Action("Home", "Home")),
+        new Tuple<string, string>("Chat", "")
+    };
+            return View(breadcrumbs);
+        }
+    }
+}
